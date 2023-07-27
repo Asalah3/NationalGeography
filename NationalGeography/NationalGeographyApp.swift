4afd11404bfcf0c3a4f6b765aa9d6864c68941fa
Asalah3 <asalahsayed3@gmail.com>
2023-07-27 17:01:42 +0300

    Initial Commit

---
+++ NationalGeography/NationalGeographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalGeographyApp.swift
+//  NationalGeography
+//
+//  Created by Asalah Sayed on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NationalGeographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
